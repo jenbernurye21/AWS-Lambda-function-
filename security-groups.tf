6ca41069ab0285d4e459bd313a48fa50d700fa83
jenbernurye21 <nuryejenber@gmail.com>
2026-03-24 10:33:32 -0500

    Update security-groups.tf

--- security-groups.tf
+++ security-groups.tf
@@ -1,1 +1,33 @@
+resource "aws_vpc" "this" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
+  tags = {
+    Name = "snapshot-cleanup-vpc"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+resource "aws_security_group" "lambda" {
+  name        = "lambda-snapshot-cleanup-sg"
+  description = "Allow outbound HTTPS"
+  vpc_id      = aws_vpc.this.id
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
